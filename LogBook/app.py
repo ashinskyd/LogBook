549bf1cf5622c5e65affbf10c86fc876f1773efd
David Ashinsky <ashinskyd@live.com>
2015-04-22 17:09:38 -0500

    Here

--- LogBook/app.py
+++ LogBook/app.py
@@ -4,18 +4,17 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask, request, redirect, render_template, flash,url_for,g
-from flask_login import (LoginManager, login_required, login_user, 
-                         current_user, logout_user, UserMixin)
+from flask_login import (LoginManager, login_required, login_user, login_url,
+                         current_user, logout_user, UserMixin, fresh_login_required)
 import redis
 from datetime import timedelta
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SET T0 4NY SECRET KEY L1KE RAND0M H4SH'
 wsgi_app = app.wsgi_app
-app.permanent_session_lifetime = timedelta(seconds=10)
-
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 class UserNotFoundError(Exception):
     pass
@@ -23,7 +22,7 @@ class UserNotFoundError(Exception):
 
 class User(UserMixin):
     USERS = {
-        'a': 'b'}
+        'a': 'c'}
 
     def __init__(self, id):
         if not id in self.USERS:
@@ -56,9 +55,7 @@ def index():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    if (current_user):
-        return "AAA"
-    elif request.method == 'GET':
+    if request.method == 'GET':
         return render_template("Login.html")
     else:
         username = request.form['username']
@@ -66,9 +63,9 @@ def login():
         u = User.get(username)
         if (u and u.password == password):
             login_user(u,remember=True)
-            u.authenticated = True
             return redirect(url_for('dataEntry'))
         else:
+            flash("Sorry, please try another login")
             return render_template("Login.html",error=True)
 
 @app.route('/dataEntry',methods=['GET'])
@@ -76,24 +73,26 @@ def login():
 def dataEntry():
     return render_template("DataEntry.html")
 
-@app.route('/displayData',methods=['POST'])
+@app.route('/displayData',methods=['POST','GET'])
 @login_required
 def displayData():
     hours  = request.form['hours']
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     oldval = r.get('hours')
+    aircraft = request.form['aircraft']
+    date = request.form['date']
+    print(date)
     if oldval is None:
         r.set('hours',hours) 
     else:
         r.set('hours',hours+oldval)
-    return render_template("DisplayData.html",hours=r.get('hours'))
+    return render_template("DisplayData.html",hours=r.get('hours'), aircraft=aircraft,date=date)
 
 @app.route('/logout',methods=['GET'])
 @login_required
 def logout():
-    user = current_user
-    user.authenticated=False
     logout_user()
+    flash("Thank you for logging out")
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
